a4b29cd27dcf6adcedf262e1eed7ac6c80fcaff5
hklim1 <xheatherlim@gmail.com>
2024-02-08 16:28:58 -0600

    Finalized the Welcome Screen, began work on the Registration and Login screens, and implemented Google Firebase with authentication.

---
+++ src/app/screens/FakePage.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const FakePage = () => {
+  return (
+    <View>
+      <Text>WELCOME TO THE FAKE HOMEPAGE</Text>
+    </View>
+  );
+};
--- src/app/screens/LoginScreen.tsx
+++ src/app/screens/LoginScreen.tsx
@@ -1,19 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { Link, Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Button, ThemeProvider } from "@rneui/themed";
 import { AppTheme, useStyles } from "../themes";
+import { firebaseAuth } from "../../../FirebaseConfig";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const auth = firebaseAuth;
+
+  const signIn = async () => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      console.log(response);
+      // alert("Incorrect email or password")
+    } catch (error: any) {
+      console.log(error);
+      alert("Login failed: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signUp = async () => {
+    setLoading(true);
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(response);
+      // alert("Incorrect email or password")
+    } catch (error: any) {
+      console.log(error);
+      alert("Registration failed: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <KeyboardAvoidingView behavior="padding">
+        <TextInput
+          value={email}
+          style={styles.input}
+          placeholder="Email"
+          autoCapitalize="none"
+          onChangeText={(text) => setEmail(text)}
+        ></TextInput>
+        <TextInput
+          value={password}
+          secureTextEntry={true}
+          style={styles.input}
+          placeholder="Password"
+          autoCapitalize="none"
+          onChangeText={(text) => setPassword(text)}
+        ></TextInput>
+        {loading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <>
+            <Button title="Login" onPress={signIn}></Button>
+            <Button title="Create Account" onPress={signUp}></Button>
+          </>
+        )}
+      </KeyboardAvoidingView>
+    </View>
+  );
+}
 
 export function LoginScreen() {
   const themeStyle = useStyles();
   return (
     <View style={styles.container}>
-      <Stack.Screen options={{ title: "Home" }} />
       <View style={styles.basicContainer}>
         <Text>Open up App.tsx to start working on your app!</Text>
-        <Link href="/Settings" style={{ margin: 20, color: "blue" }}>
+        <Link
+          href="screens/WelcomeScreen"
+          style={{ margin: 20, color: "blue" }}
+        >
           Settings
         </Link>
       </View>
@@ -41,6 +125,11 @@ const styles = StyleSheet.create({
     paddingVertical: 100,
     // position: "absolute",
     // top: 70,
+  },
+  input: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
   },
 });
 
