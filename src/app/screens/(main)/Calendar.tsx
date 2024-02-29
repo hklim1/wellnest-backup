2dc43253a12bd2285ff1bf3afd0ef2f4a0568d16
hklim1 <xheatherlim@gmail.com>
2024-02-28 22:51:48 -0600

    Fixing calendar after merge issue

--- src/app/screens/(main)/Calendar.tsx
+++ src/app/screens/(main)/Calendar.tsx
@@ -1,9 +1,9 @@
 import {
-    Text,
-    View,
-    StyleSheet,
-    FlatList,
-    ActivityIndicator,
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
 } from "react-native";
 import { Stack } from "expo-router";
 import { usePathname, useRouter } from "expo-router";
@@ -13,9 +13,9 @@ import { useState, useEffect } from "react";
 import Members from "../../../components/Members";
 import members, { MemberType } from "../../../lib/members";
 import {
-    getAppointments,
-    getDependentIcons,
-    getDependents,
+  getAppointments,
+  getDependentIcons,
+  getDependents,
 } from "../../utils/firebaseUtils";
 import { Badge, ListItem } from "@rneui/themed";
 import data, { AppointmentType } from "../../../lib/appointments";
@@ -24,59 +24,59 @@ import { MarkedDates } from "react-native-calendars/src/types";
 import HeaderRight from "../../../components/HeaderRight";
 import { getUserId } from "../../utils/globalStorage";
 const last = {
-    id: "fakeID",
-    name: "All",
-    firstName: "All",
-    icon: "all",
-    email: "maria@gmail.com",
-    color: "#0FA6B0",
+  id: "fakeID",
+  name: "All",
+  firstName: "All",
+  icon: "all",
+  email: "maria@gmail.com",
+  color: "#0FA6B0",
 };
 
 const CalendarScreen = () => {
-    const path = usePathname();
-    const router = useRouter();
-    const [member, setMember] = useState("fakeID");
-    const [day, setDay] = useState("");
-    const [markDates, setMarkDates] = useState<MarkedDates>({});
-    const [dependents, setDependents] = useState({});
-    const [loading, setLoading] = useState(false);
-    const [appointments, setAppointments] = useState<AppointmentType[]>();
+  const path = usePathname();
+  const router = useRouter();
+  const [member, setMember] = useState("fakeID");
+  const [day, setDay] = useState("");
+  const [markDates, setMarkDates] = useState<MarkedDates>({});
+  const [dependents, setDependents] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [appointments, setAppointments] = useState<AppointmentType[]>();
 
-    useEffect(() => {
-        setLoading(true);
-        const getInfo = async () => {
-            const id = await getUserId();
-            if (id) {
-                const deps = await getDependents(id);
-                console.log(deps);
-                const result = await getDependentIcons(Object.keys(deps));
-                console.log("============ result is", result);
-                if (result) {
-                    setDependents({ ...result, LAST: last });
-                }
-            } else {
-                console.warn("ID dont exist");
-            }
-        };
-        const getData = async () => {
-            const result = await getAppointments();
-            setAppointments(Object.values(result));
-            console.log(Object.values(result));
-        };
+  useEffect(() => {
+    setLoading(true);
+    const getInfo = async () => {
+      const id = await getUserId();
+      if (id) {
+        const deps = await getDependents(id);
+        console.log(deps);
+        const result = await getDependentIcons(Object.keys(deps));
+        console.log("============ result is", result);
+        if (result) {
+          setDependents({ ...result, LAST: last });
+        }
+      } else {
+        console.warn("ID dont exist");
+      }
+    };
+    const getData = async () => {
+      const result = await getAppointments();
+      setAppointments(Object.values(result));
+      console.log(Object.values(result));
+    };
 
-        getData();
-        getInfo();
-        setLoading(false);
-    }, []);
+    getData();
+    getInfo();
+    setLoading(false);
+  }, []);
 
   const getMarkedDates = () => {
     const markedDates: MarkedDates = {};
 
-        appointments?.forEach((info, index) => {
-            const dateKey = info.formattedDate;
-            const memberId = info.memberId;
-            const colors = ["red", "green", "blue", "orange", "purple"];
-            const color = colors[Math.floor(Math.random() * colors.length)];
+    appointments?.forEach((info, index) => {
+      const dateKey = info.formattedDate;
+      const memberId = info.memberId;
+      const colors = ["red", "green", "blue", "orange", "purple"];
+      const color = colors[Math.floor(Math.random() * colors.length)];
 
       // If the dateKey is not present in markedDates, initialize it with an empty dots object
       if (!markedDates[dateKey]) {
@@ -89,15 +89,13 @@ const CalendarScreen = () => {
         };
       }
 
-            // Add a dot to the dots array for the current member
-            if (markedDates[dateKey]?.dots) {
-                // Add a dot to the dots array for the current member
-                markedDates[dateKey]!.dots!.push({
-                    key: `${dateKey}-${memberId} ${Math.random() * 10}`,
-                    color: color,
-                    selectedDotColor: color,
-                });
-            }
+      // Add a dot to the dots array for the current member
+      if (markedDates[dateKey]?.dots) {
+        // Add a dot to the dots array for the current member
+        markedDates[dateKey]!.dots!.push({
+          key: `${dateKey}-${memberId} ${Math.random() * 10}`,
+          color: color,
+          selectedDotColor: color,
         });
       }
     });
@@ -109,44 +107,33 @@ const CalendarScreen = () => {
     getMarkedDates();
   }, []);
 
-  // console.log(markDates);
+  console.log(markDates);
 
-    if (loading) {
-        return (
-            <View style={{ flex: 1 }}>
-                <ActivityIndicator size={32} />
-            </View>
-        );
-    }
-
+  if (loading) {
     return (
-        <View style={styles.container}>
-            <Stack.Screen
-                options={{
-                    title: "Calendar",
-                    headerTitleStyle: {
-                        fontFamily: "Inter600",
-                        fontSize: 16,
-                    },
-                    headerTitleAlign: "center",
-                    headerShadowVisible: false,
-                    headerStyle: {
-                        backgroundColor: "#F5F7F7",
-                    },
-                    headerRight: () => (
-                        <View style={{ paddingRight: 16 }}>
-                            <HeaderRight />
-                        </View>
-                    ),
-                }}
-            />
+      <View style={{ flex: 1 }}>
+        <ActivityIndicator size={32} />
+      </View>
+    );
+  }
 
-            <View>
-                <Members
-                    members={Object.values(dependents)}
-                    setActiveMember={setMember}
-                    activeMember={member}
-                />
+  return (
+    <View style={styles.container}>
+      <Stack.Screen
+        options={{
+          title: "Calendar",
+          headerTitleStyle: {
+            fontFamily: "Inter600",
+            fontSize: 16,
+          },
+          headerTitleAlign: "center",
+          headerShadowVisible: false,
+          headerStyle: {
+            backgroundColor: "#F5F7F7",
+          },
+          headerRight: () => (
+            <View style={{ paddingRight: 16 }}>
+              <HeaderRight />
             </View>
           ),
         }}
@@ -154,7 +141,7 @@ const CalendarScreen = () => {
 
       <View>
         <Members
-          members={[...members, last]}
+          members={Object.values(dependents)}
           setActiveMember={setMember}
           activeMember={member}
         />
@@ -165,46 +152,39 @@ const CalendarScreen = () => {
           markingType="multi-dot"
           onDayPress={(date) => {
             setDay(date.dateString);
-            // console.log(date.dateString, "DATETTETTE");
+            console.log(date.dateString, "DATETTETTE");
           }}
           markedDates={markDates}
         />
       </View>
 
-            {Object.keys(dependents).length >= 1 && (
-                <FlatList
-                    showsVerticalScrollIndicator={false}
-                    contentContainerStyle={{
-                        borderRadius: 20,
-                        backgroundColor: "white",
-                        overflow: "hidden",
-                    }}
-                    data={
-                        member == "fakeID"
-                            ? appointments
-                            : appointments?.filter(
-                                  (item) => item.memberId == member
-                              )
-                    }
-                    renderItem={({ item }) => {
-                        const userIcon = dependents[item.memberId].icon;
-                        return (
-                            <ListItem
-                                bottomDivider
-                                containerStyle={{ padding: 0 }}>
-                                <ListItem.Content>
-                                    <AppointmentCard
-                                        data={item}
-                                        icon={userIcon}
-                                    />
-                                </ListItem.Content>
-                            </ListItem>
-                        );
-                    }}
-                />
-            )}
-        </View>
-    );
+      {Object.keys(dependents).length >= 1 && (
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            borderRadius: 20,
+            backgroundColor: "white",
+            overflow: "hidden",
+          }}
+          data={
+            member == "fakeID"
+              ? appointments
+              : appointments?.filter((item) => item.memberId == member)
+          }
+          renderItem={({ item }) => {
+            const userIcon = dependents[item.memberId].icon;
+            return (
+              <ListItem bottomDivider containerStyle={{ padding: 0 }}>
+                <ListItem.Content>
+                  <AppointmentCard data={item} icon={userIcon} />
+                </ListItem.Content>
+              </ListItem>
+            );
+          }}
+        />
+      )}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
